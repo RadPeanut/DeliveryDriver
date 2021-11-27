50e289a4f3fcab4f154c16d2ec82c3938a42e4e9
Chris Armstrong <dev.radpeanut@gmail.com>
2021-11-27 11:44:41 -0600

    Added world objects and collisions

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -7,4 +7,8 @@ public class Collision : MonoBehaviour
     void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Ow! My Toe!");
     }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("I feel triggered!");
+    }
 }
